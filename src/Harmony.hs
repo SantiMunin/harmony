d51040c19d42a499641c4e0c68fc1ec888017f5b
Santiago Munín <santimunin@gmail.com>
2015-02-11 17:07:59 +0100

    Fix style

--- src/Harmony.hs
+++ src/Harmony.hs
@@ -1,22 +1,22 @@
 module Main where
 
-import System.Environment
+import           System.Environment
 
-import Language.ErrM
-import Language.Abs
-import Language.Lex
-import Language.Par
-import Language.Print
+import           Language.Abs
+import           Language.ErrM
+import           Language.Lex
+import           Language.Par
+import           Language.Print
 
 main :: IO ()
-main = do 
+main = do
   args <- getArgs
   case args of
        [file] -> do
          contents <- readFile file
          case pSpecification (myLexer contents) of
-           Bad err -> error $ show err 
+           Bad err -> error $ show err
            Ok tree -> putStrLn $ printTree tree
-       args -> error $ "Usage: harmony <source_file>" 
+       args -> error "Usage: harmony <source_file>"
   return ()
 
