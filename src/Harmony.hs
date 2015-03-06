5f007f4cac8d76aa46f93ed46f7cfce6162697a6
Santiago Munín <santimunin@gmail.com>
2015-03-06 19:45:12 +0100

    Change flags to use records.
      * Now output dir is an optional flag (instead of an argument)
      * Fix #13

--- src/Harmony.hs
+++ src/Harmony.hs
@@ -12,18 +12,50 @@ import           System.Environment         (getArgs, getProgName)
 import           System.Exit                (ExitCode (ExitFailure), exitWith)
 
 -- | Flags of the executable
-data Flag = Client String
-          | Server String
-          | OutputDir String
-          deriving (Eq, Show)
+data Options = Options
+  { targets   :: [Target]
+  , outputDir :: FilePath
+  } deriving Show
+
+-- | Supported implementations (the preffix 'C' stands for 'Client' where as 'S' stands for
+-- 'Server').
+data Target =
+    CPython
+  | CJavascript
+  | SJavascript
+  deriving Show
+
+defaultOptions :: Options
+defaultOptions = Options
+  { targets = []
+  , outputDir = "."
+  }
 
 -- | Definition of the flags expected by the executable
-options :: [OptDescr Flag]
+options :: [OptDescr (Options -> Options)]
 options = [ Option ['c'] ["client"]
-                   (OptArg (Client . fromJust) "CLIENT") "Desired output for the client"
+                   (OptArg (\c options -> options { targets = parseClient c:targets options})
+                           "CLIENTS")
+                   "Desired output for the client"
           , Option ['s'] ["server"]
-                   (OptArg (Server . fromJust) "SERVER") "Desired output for the server"
+                   (OptArg (\s options -> options { targets = parseServer s:targets options})
+                           "SERVERS")
+                   "Desired output for the server"
+          , Option ['o'] ["output_dir"]
+                   (OptArg (\dir options -> options { outputDir = fromJust dir })
+                           "OUTPUT_DIR")
+                   "Output path"
           ]
+
+-- | Parse flag input
+parseClient, parseServer :: Maybe String -> Target
+parseClient (Just "js") = CJavascript
+parseClient (Just "python") = CPython
+parseClient (Just other) = error $ "Could not parse client: " ++ other
+parseClient Nothing = error "parseClient: not expected Nothing as flag"
+parseServer (Just "js") = SJavascript
+parseServer (Just other) = error $ "Could not parse server" ++ other
+parseServer Nothing = error "parseServer: not expected Nothing as flag"
 
 -- | Entry point, parses arguments, then the specification file and generates all the desired
 -- | outputs.
@@ -45,18 +77,18 @@ parseArgs = do
   programName <- getProgName
 
   case getOpt Permute options args of
-    (flags, paths, []) -> do
-      unless (length paths == 2)
+    (flags, args, []) -> do
+      unless (length args == 1)
              (printUsageAndExitWithError Nothing programName options)
       unless (not $ null flags)
              (printUsageAndExitWithError (Just "Select at least a client/server") programName options)
-      return (head paths, paths !! 1, map getGenInfo $ filter isLanguageOutput flags)
+      return (head args, outputDir info, desiredOutputs)
         where
-          isLanguageOutput (OutputDir _) = False
-          isLanguageOutput _ = True
+          info :: Options
+          info = foldl (\record f -> f record)  defaultOptions flags
+          desiredOutputs = map getGenInfo $ targets info
     -- TODO: check if this should be another printUsageAndExitWithError call
     (_, _, errs) -> ioError (userError (concat errs ++ usage programName options))
-
 
 -- | Generate all the output required.
 generateOutput :: Specification -> [OG.GenerationInfo] -> FilePath -> IO ()
@@ -65,8 +97,8 @@ generateOutput spec genInfos outputPath = forM_ genInfos (OG.generateOutput outp
 -- TODO: think about moving this to a different module to centralize the specification of all the
 -- implemented outputs.
 -- | Maps flags to generation information.
-getGenInfo :: Flag -> OG.GenerationInfo
-getGenInfo (Server "js") = OG.createGenInfo files templates fieldMapping
+getGenInfo :: Target -> OG.GenerationInfo
+getGenInfo SJavascript = OG.createGenInfo files templates fieldMapping
   where
     files = []
     templates = [ ("templates/server/js/server.tpl", "js")
@@ -78,13 +110,13 @@ getGenInfo (Server "js") = OG.createGenInfo files templates fieldMapping
 getGenInfo other = error $ "Couldn't process " ++ show other ++ " flag."
 
 -- | Generates the usage message.
-usage :: String -> [OptDescr Flag] -> String
+usage :: String -> [OptDescr (Options -> Options)] -> String
 usage progName = usageInfo header
   where
-    header = "Usage: " ++ progName ++ " [OPTION...] input_file output_path"
+    header = "Usage: " ++ progName ++ " [OPTION...] input_file"
 
 -- | Prints usage and exits with error.
-printUsageAndExitWithError :: Maybe String -> String -> [OptDescr Flag] -> IO ()
+printUsageAndExitWithError :: Maybe String -> String -> [OptDescr (Options -> Options)] -> IO ()
 printUsageAndExitWithError message progName options = do
   case message of
     Just str -> putStrLn $ "ERROR: " ++ str ++ "\n"
