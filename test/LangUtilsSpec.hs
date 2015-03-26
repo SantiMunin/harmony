22d2601c7d1329ad53fa676b45b4a27fc3fb42a1
Santiago Munín <santimunin@gmail.com>
2015-03-26 01:00:29 +0100

    Adapt some of the test to QuickCheck properties and make Specification implement Arbitrary

--- test/LangUtilsSpec.hs
+++ test/LangUtilsSpec.hs
@@ -1,65 +1,67 @@
 module LangUtilsSpec where
 
 import           ApiSpec
-import qualified Data.Map     as M
+import qualified Data.Map        as M
 import           Language.Abs
 import           LangUtils
 import           Test.Hspec
+import           Test.QuickCheck
+import           TestUtils       ()
 
 spec :: Spec
 spec =
   describe "LangUtilsSpec" $ do
     context "specName" $
-      it "returns the spec name" $
-        specName mockSpec `shouldBe` "name"
+      it "returns the spec name" $ property $
+        \spec@(Spec (Nm (Ident name)) _ _ _ _) -> specName spec == name
 
     context "specVersion" $
-      it "returns the spec version" $
-        specVersion mockSpec `shouldBe` "version"
+      it "returns the spec version" $ property $
+        \spec@(Spec _ (Ver (VerIdent version)) _ _ _) -> specVersion spec == version
 
     context "enumName" $
-      it "returns the name of the enum" $
-        enumName mockEnum `shouldBe` "EnumName"
+      it "returns the name of the enum" $ property $
+        \enum@(DefEnum (Ident name) _ ) -> enumName enum == name
 
     context "enumVals" $
-      it "returns the list of values of an enum" $
-        enumVals mockEnum `shouldBe` mockEnumVals
+      it "returns the list of values of an enum" $ property $
+        \enum@(DefEnum _ vals) -> enumVals enum == vals
 
     context "enumValName" $
-      it "returns the name of the value (unwraps it)" $
-        enumValName (EnVal (Ident "ENUM_VAL_NAME")) `shouldBe` "ENUM_VAL_NAME"
+      it "returns the name of the value (unwraps it)" $ property $
+        \enumVal@(EnVal (Ident valName)) -> enumValName enumVal == valName
 
     context "strName" $
-      it "returns the name of the struct" $
-        strName mockStruct `shouldBe` "StrName"
+      it "returns the name of the struct" $ property $
+        \str@(DefStr (Ident name) _ ) -> strName str == name
 
     context "strFields" $
-      it "returns the fields of the struct" $
-        strFields mockStruct `shouldBe` mockFields
+      it "returns the fields of the struct" $ property $
+        \str@(DefStr _ fields) -> strFields str == fields
 
     context "resName" $
-      it "returns the name of a resource (which is the struct it refers)" $
-        resName mockResource `shouldBe` "ResourceName"
+      it "returns the name of a resource (which is the struct it refers)" $ property $
+        \res@(Res (Ident name) _ _) -> resName res == name
 
     context "resRoute" $
-      it "returns the route of a resource" $
-        resRoute mockResource `shouldBe` "/route"
+      it "returns the route of a resource" $ property $
+        \res@(Res _ route _) -> resRoute res == route
 
     context "resIsWritable" $
-      it "returns the writing mode of a resource" $
-        resIsWritable mockResource `shouldBe` True
+      it "returns the writing mode of a resource" $ property $
+        \res@(Res _ _ mode) -> resIsWritable res == (mode == Write)
 
     context "fieldName" $
-      it "returns the name of a field from the AST" $
-        fieldName mockField `shouldBe` "Field"
+      it "returns the name of a field from the AST" $ property $
+        \field@(FDef _ (Ident name) _) -> fieldName field == name
 
     context "fieldAnnotations" $
-      it "returns the annotations of a field from the AST" $
-        fieldAnnotations mockField `shouldBe` mockAnnotations
+      it "returns the annotations of a field from the AST" $ property $
+        \field@(FDef ann _ _) -> fieldAnnotations field == ann
 
     context "fieldType" $
-      it "returns the type of a field from the AST" $
-        fieldType mockField `shouldBe` FString
+      it "returns the type of a field from the AST" $ property $
+        \field@(FDef _ _ ftype) -> fieldType field == ftype
 
     context "fieldSpecType" $ do
       it "returns ApiSpec.TString from Language.Abs.TString" $
@@ -84,14 +86,6 @@ spec =
         fieldSpecType mockApiSpec (FList (FDefined (Ident "AnEnum"))) `shouldBe` TList (TEnum "AnEnum")
 
   where
-    mockSpec = Spec (Nm (Ident "name")) (Ver (VerIdent "version")) [] [] []
-    mockEnum = DefEnum (Ident "EnumName") mockEnumVals
-    mockEnumVals = [EnVal (Ident "value1"), EnVal (Ident "value2")]
-    mockStruct = DefStr (Ident "StrName") mockFields
-    mockFields = [mockField]
-    mockResource = Res (Ident "ResourceName") "/route" Write
-    mockField = FDef mockAnnotations (Ident "Field") FString
-    mockAnnotations = [Ann (Ident "PrimaryKey"), Ann (Ident "Hidden")]
     mockApiSpec = AS { name = "name"
                      , version = "version"
                      , enums = M.insert "AnEnum" mockEnumInfo M.empty
--- test/TestUtils.hs
+++ test/TestUtils.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module TestUtils (assertException) where
 
 import           Control.Exception
 import           Control.Monad
+import           Data.DeriveTH
+import           Language.Abs
 import           Test.HUnit
+import           Test.QuickCheck
 
 -- | Asserts that some computation throws a concrete error.
 assertException :: (Exception e, Eq e) => e -> IO a -> IO ()
@@ -11,3 +16,18 @@ assertException ex action =
   action
   assertFailure $ "Expected exception: " ++ show ex
   where isWanted = guard . (== ex)
+
+-- TODO(#42)
+derive makeArbitrary ''Ident
+derive makeArbitrary ''Name
+derive makeArbitrary ''Version
+derive makeArbitrary ''VerIdent
+derive makeArbitrary ''Specification
+derive makeArbitrary ''EnumType
+derive makeArbitrary ''EnumVal
+derive makeArbitrary ''StructType
+derive makeArbitrary ''Field
+derive makeArbitrary ''Annotation
+derive makeArbitrary ''FType
+derive makeArbitrary ''Resource
+derive makeArbitrary ''Mode
